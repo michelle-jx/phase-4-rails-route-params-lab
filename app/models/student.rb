bf4dfcc9820ca5ffd7eaeb40de01e8388a0f5615
Michelle Too <mjxtcodes@gmail.com>
2022-12-12 09:50:05 -0800

    commit tree 0b0d8f68baefaf2a0f5a95b54c4954ab145767d8

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def self.by_name(name)
+    Student.where(first_name.include?(name)).or(Student.where(last_name.include?(name)))
+  end
+  
 end
